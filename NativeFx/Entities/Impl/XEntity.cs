8b0dcb9ff9e269fc2025a585d768bc60fdaf6bcc
WithLithum <WithLithum@foxmail.com>
2022-11-17 10:11:03 +0800

    🚧 Additional XEntity WIP

--- NativeFx/Entities/Impl/XEntity.cs
+++ NativeFx/Entities/Impl/XEntity.cs
@@ -30,8 +30,36 @@ public abstract class XEntity : IHandleable, IDeletable, IPersistable, IFreezabl
         Handle = handle;
     }
 
+    public bool CanBeDamaged
+    {
+        get => Natives.GetEntityCanBeDamaged(Handle);
+        set => Natives.SetEntityCanBeDamaged(Handle, value);
+    }
+
+    public System.Numerics.Vector3 ForwardVector
+    {
+        get
+        {
+            var nat = Natives.GetEntityForwardVector(Handle);
+            return new System.Numerics.Vector3(nat.X, nat.Y, nat.Z);
+        }
+    }
+
+    public System.Numerics.Vector2 Forward2D
+    {
+        get
+        {
+            return new System.Numerics.Vector2(ForwardX, ForwardY);
+        }
+    }
+
+    public float ForwardX => Natives.GetEntityForwardX(Handle);
+    public float ForwardY => Natives.GetEntityForwardY(Handle);
+
     /// <inheritdoc />
     public int Handle { get; protected internal set; }
+
+    public float HeightAboveGround => Natives.GetEntityHeightAboveGround(Handle);
 
     /// <summary>
     /// Gets a value indicating whether this instance is still alive.
@@ -42,6 +70,22 @@ public abstract class XEntity : IHandleable, IDeletable, IPersistable, IFreezabl
     /// Gets a value indicating whether this instance is dead.
     /// </summary>
     public bool IsDead => Natives.IsEntityDead(Handle, false);
+
+    public bool IsInAir => Natives.IsEntityInAir(Handle);
+
+    /// <summary>
+    /// Gets a value indicating whether this instance is in water.
+    /// </summary>
+    public bool IsInWater => Natives.IsEntityInWater(Handle);
+
+    /// <summary>
+    /// Gets a value indicating whether this instance is static.
+    /// </summary>
+    /// <remarks>
+    /// A static instance will not respond to task invoking and manipulation regarding physics
+    /// of static instances are restricted.
+    /// </remarks>
+    public bool IsStatic => Natives.IsEntityStatic(Handle);
 
     /// <inheritdoc />
     public bool IsPersistent
@@ -75,6 +119,13 @@ public abstract class XEntity : IHandleable, IDeletable, IPersistable, IFreezabl
         }
     }
 
+    /// <summary>
+    /// Gets the physical heading of this instance in degrees.
+    /// </summary>
+    public float PhysicsHeading => Natives.GetEntityHeadingFromEulers(Handle);
+
+    public float Pitch => Natives.GetEntityPitch(ValidHandleOrException());
+
     /// <inheritdoc />
     public System.Numerics.Vector3 Position
     {
@@ -96,6 +147,26 @@ public abstract class XEntity : IHandleable, IDeletable, IPersistable, IFreezabl
         }
         set => Natives.SetEntityRotation(Handle, value.X, value.Y, value.Z, 2, true);
     }
+
+    /// <summary>
+    /// Gets a value indicating whether this instance have drawable model.
+    /// </summary>
+    public bool HaveDrawable => Natives.DoesEntityHaveDrawable(Handle);
+
+    /// <summary>
+    /// Gets a value indicating whether this instance have physics.
+    /// </summary>
+    public bool HavePhysics => Natives.DoesEntityHavePhysics(Handle);
+
+    /// <summary>
+    /// Gets a value indicating whether this instance have skeleton.
+    /// </summary>
+    /// <remarks>
+    /// This property is introduced in game build 2699.
+    /// </remarks>
+    public bool HaveSkeleton => Natives.DoesEntityHaveSkeleton(Handle);
+
+    public bool HaveAnimationDirector => Natives.DoesEntityHaveAnimDirector(Handle);
 
     /// <inheritdoc />
     public float Heading
@@ -188,6 +259,37 @@ public abstract class XEntity : IHandleable, IDeletable, IPersistable, IFreezabl
         }
     }
 
+    protected int ValidHandleOrException()
+    {
+        if (!IsValid())
+        {
+            throw new InvalidOperationException("The operation is not valid because this instance is invalid.");
+        }
+
+        return Handle;
+    }
+
+    public bool IsAttachedTo(XEntity other)
+    {
+        if (other == null) throw new ArgumentNullException(nameof(other));
+        if (!other.IsValid()) throw new ArgumentException("The other entity is invalid.", nameof(other));
+
+        return Natives.IsEntityAttachedToEntity(ValidHandleOrException(), other.Handle);
+    }
+
+    /// <summary>
+    /// Determines whether this instance is in screen space.
+    /// </summary>
+    /// <remarks>
+    /// This method does not check if the player can actually see this instance on screen; instead, this method just checks if this instance
+    /// is anywhere in the screen space.
+    /// </remarks>
+    /// <returns><see langword="true"/> if this instance is in screen space; otherwise, <see langword="false"/>.</returns>
+    public bool IsOnScreen()
+    {
+        return Natives.IsEntityOnScreen(ValidHandleOrException());
+    }
+
     /// <inheritdoc />
     public bool IsValid()
     {
@@ -215,6 +317,13 @@ public abstract class XEntity : IHandleable, IDeletable, IPersistable, IFreezabl
         Natives.SetEntityAsMissionEntity(Handle, false, false);
     }
 
+    public void ProcessAttachments() => Natives.ProcessEntityAttachments(ValidHandleOrException());
+
+    public void SetAnimationSpeed(string animDict, string anim, float multiplier) =>
+        Natives.SetEntityAnimSpeed(Handle, animDict, anim, multiplier);
+
+    public void StopAnimation(string animDict, string anim) => Natives.StopEntityAnim(ValidHandleOrException(), anim, animDict, 1f);
+
     /// <inheritdoc />
     public void Freeze(bool freeze)
     {
@@ -228,5 +337,38 @@ public abstract class XEntity : IHandleable, IDeletable, IPersistable, IFreezabl
     public void SetOnlyDamagedByPlayer(bool toggle)
     {
         Natives.SetEntityOnlyDamagedByPlayer(Handle, toggle);
+    }
+
+    /// <summary>
+    /// Determines whether this instance has or had been damaged by the specified <paramref name="other"/> entity.
+    /// </summary>
+    /// <param name="other">The entity to check.</param>
+    /// <returns><see langword="true"/> if this instance has or had been damaged by <paramref name="other"/> entity; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException">The specified <paramref name="other"/> entity is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentException">The specified <paramref name="other"/> entity is not valid.</exception>
+    public bool HasBeenDamagedBy(XEntity other)
+    {
+        if (other == null) throw new ArgumentNullException(nameof(other));
+        if (!other.IsValid()) throw new ArgumentException("The other entity is invalid.", nameof(other));
+
+        return Natives.HasEntityBeenDamagedByEntity(ValidHandleOrException(), other.Handle, true);
+    }
+
+    /// <summary>
+    /// Determines whether this instance is currently colliding or collided with anything.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// This method checks whether this instance is collided with anything (ground, entities, &amp;c.) for the current tick.
+    /// </para>
+    /// <note type="note">
+    /// For vehicles, this method will return <see langword="false"/> even if the wheels of it touches the ground, as this method will only
+    /// determine whether the body has collided with anything (except wheels, &amp;c. on vehicle itself) when checking vehicels.
+    /// </note>
+    /// </remarks>
+    /// <returns><see langword="true"/> if this instance is currently colliding or collided with anything; otherwise, <see langword="false"/>.</returns>
+    public bool HasCollidedWithAnything()
+    {
+        return Natives.HasEntityCollidedWithAnything(Handle);
     }
 }
