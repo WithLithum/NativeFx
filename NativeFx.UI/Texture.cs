b3e0584d95077ff2ff05cce1ede28d8a7c07c3ed
WithLithum <WithLithum@foxmail.com>
2022-11-21 14:42:39 +0800

    ✨ Add NativeFx UI Module

---
+++ NativeFx.UI/Texture.cs
@@ -0,0 +1,52 @@
+﻿namespace NativeFx.UI;
+
+using GTA;
+
+public class Texture
+{
+    public Texture(string dictionary, string name)
+    {
+        Dictionary = dictionary;
+        Name = name;
+    }
+
+    /// <summary>
+    /// Gets the dictionary of this instance.
+    /// </summary>
+    public string Dictionary { get; }
+
+    /// <summary>
+    /// Gets the name of this instance.
+    /// </summary>
+    public string Name { get; }
+
+    /// <summary>
+    /// Loads the dictionary of this instance.
+    /// </summary>
+    public void Load()
+    {
+        if (!Natives.HasStreamedTextureDictLoaded(Dictionary))
+        {
+            Natives.RequestStreamedTextureDict(Dictionary, true);
+        }
+    }
+
+    /// <summary>
+    /// Loads the dictionary of this instance, and waits indefinitely for it to complete.
+    /// </summary>
+    /// <remarks>
+    /// <note type="warning">
+    /// You must call this method inside the ticking routine (aka the Tick event or anything called in its handler)
+    /// of a script instead of its constructor. It is illegal to call this method inside script constructors.
+    /// </note>
+    /// </remarks>
+    public void LoadAndWait()
+    {
+        Load();
+
+        while (!Natives.HasStreamedTextureDictLoaded(Dictionary))
+        {
+            Script.Yield();
+        }
+    }
+}
---
+++ NativeFx.UI/XNotification.cs
@@ -0,0 +1,61 @@
+﻿namespace NativeFx.UI;
+
+/// <summary>
+/// Provides methods for displaying various types of notifications.
+/// </summary>
+public static class XNotification
+{
+    /// <summary>
+    /// Sets the background colour of the next post.
+    /// </summary>
+    /// <remarks>
+    /// This method must be called before each notification showing call to set background colour.
+    /// </remarks>
+    /// <param name="hudColour">The ID of the HUD colour. This ID is the zero-based index of the colour in <c>hudcolor.dat</c>.</param>
+    internal static void SetNextBackgroundColour(int hudColour)
+    {
+        Natives.ThefeedSetBackgroundColorForNextPost(hudColour);
+    }
+
+    internal static void BeginCommand(string text)
+    {
+        Natives.BeginTextCommandThefeedPost("STRING");
+        Natives.AddTextComponentSubstringPlayerName(text);
+    }
+
+    /// <summary>
+    /// Hides the specified notification.
+    /// </summary>
+    /// <param name="notification">The ID of the notification to hide.</param>
+    public static void Hide(int notification)
+    {
+        Natives.ThefeedRemoveItem(notification);
+    }
+
+    /// <summary>
+    /// Shows a simple notification with the specified text.
+    /// </summary>
+    /// <param name="text">The text.</param>
+    /// <param name="blink">If <see langword="true"/>, blinks for a few times when the notification is shown.</param>
+    /// <returns>The ID of the notification created.</returns>
+    public static int Show(string text, bool blink = false)
+    {
+        BeginCommand(text);
+        return Natives.EndTextCommandThefeedPostTicker(blink, false);
+    }
+
+    /// <summary>
+    /// Shows a complicated notification with the specified text.
+    /// </summary>
+    /// <param name="texture">The texture to display.</param>
+    /// <param name="title">The title.</param>
+    /// <param name="subtitle">The subtitle.</param>
+    /// <param name="text">The text to display.</param>
+    /// <param name="blink">If <see langword="true"/>, blinks for a few times when the notification is shown.</param>
+    /// <returns>The ID of the notification created.</returns>
+    public static int Show(Texture texture, string title, string subtitle, string text, bool blink = false)
+    {
+        BeginCommand(text);
+        return Natives.EndTextCommandThefeedPostMessagetext(texture.Dictionary, texture.Name, blink, 0, title, subtitle);
+    }
+}
